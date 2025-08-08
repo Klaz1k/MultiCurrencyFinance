0495f03d4b801d8df1af237a0db1e64f52e8f013
Victor Madera <72475885+Klaz1k@users.noreply.github.com>
2025-08-08 08:19:11 -0400

    Initial commit

---
+++ multi_currency_finance/README.md
@@ -0,0 +1,3 @@
+# multi_currency_finance
+
+A new Flutter project.
---
+++ multi_currency_finance/android/app/src/main/kotlin/com/example/multi_currency_finance/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.multi_currency_finance
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
