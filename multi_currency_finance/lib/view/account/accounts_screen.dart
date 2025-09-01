f5fd8dfdc80cf088d27f98bf6722fdc9c020775a
Victor Madera <72475885+Klaz1k@users.noreply.github.com>
2025-09-01 14:31:39 -0400

    Added exchange rate to balances on Accounts Screen

--- multi_currency_finance/lib/view/account/accounts_screen.dart
+++ multi_currency_finance/lib/view/account/accounts_screen.dart
@@ -157,7 +157,7 @@ class AccountCardState extends State<AccountCard> {
                 padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
-                  children: widget.account.balance.balanceQueue.map((balanceAmount) => Text('Amount: ${balanceAmount.amount.toStringAsFixed(2)} (${(balanceAmount.amount/balanceAmount.exchangeRate).toStringAsFixed(2)})')).toList(),
+                  children: widget.account.balance.balanceQueue.map((balanceAmount) => Text('Amount: ${balanceAmount.amount.toStringAsFixed(2)} at ${balanceAmount.exchangeRate.toStringAsFixed(2)} (${(balanceAmount.amount/balanceAmount.exchangeRate).toStringAsFixed(2)})')).toList(),
                 ),
               ),
           ],
