6cf932118937a3c25858d54f24bcc4cb8c22a76a
Victor Madera <72475885+Klaz1k@users.noreply.github.com>
2026-07-25 00:44:11 -0400

    Removed generated files from tracking

--- multi_currency_finance/lib/controller/category/expense/repository/hive/hive_expense_category_entities.dart
+++ multi_currency_finance/lib/controller/category/expense/repository/hive/hive_expense_category_entities.dart
@@ -2,6 +2,8 @@ import 'package:hive/hive.dart';
 import 'package:multi_currency_finance/controller/common/hive/hive_type_constants.dart';
 import 'package:multi_currency_finance/model/category/expense/expense_category.dart';
 
+part 'hive_expense_category_entities.g.dart';
+
 @HiveType(typeId: HiveConstants.ExpenseCategoryHiveId)
 class ExpenseCategoryHiveObject extends HiveObject {
   @HiveField(0)
--- multi_currency_finance/lib/controller/category/income/repository/hive/hive_income_category_entities.dart
+++ multi_currency_finance/lib/controller/category/income/repository/hive/hive_income_category_entities.dart
@@ -2,6 +2,8 @@ import 'package:hive/hive.dart';
 import 'package:multi_currency_finance/controller/common/hive/hive_type_constants.dart';
 import 'package:multi_currency_finance/model/category/income/income_category.dart';
 
+part 'hive_income_category_entities.g.dart';
+
 @HiveType(typeId: HiveConstants.IncomeCategoryHiveId)
 class IncomeCategoryHiveObject extends HiveObject {
   @HiveField(0)
