1458c9b8effa3cbe71da435a8cc82601bd8f3a19
Victor Madera <72475885+Klaz1k@users.noreply.github.com>
2026-07-28 20:31:35 -0400

    package name update

--- multi_currency_finance/android/app/build.gradle.kts
+++ multi_currency_finance/android/app/build.gradle.kts
@@ -15,7 +15,7 @@ if (keystorePropertiesFile.exists()) {
 }
 
 android {
-    namespace = "com.multi_currency_finance.release"
+    namespace = "multi.currency.finance"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "28.2.13676358"
 
@@ -30,7 +30,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.multi_currency_finance"
+        applicationId = "multi.currency.finance"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- multi_currency_finance/android/app/src/main/kotlin/com/example/multi_currency_finance/MainActivity.kt
+++ multi_currency_finance/android/app/src/main/kotlin/com/example/multi_currency_finance/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.multi_currency_finance
+package multi.currency.finance
 
 import io.flutter.embedding.android.FlutterActivity
 
